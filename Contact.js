78d70e8e2f92bfcb4fa140675fa0314ff85fbc3d
Sierra <133829852+cececodes1@users.noreply.github.com>
2024-10-28 10:35:10 -0400

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+    return(
+        <section className="contact-section">
+            <h2>Contact Me</h2>
+            <div className="contact-content">
+                <p>Email: sierrdmichel@gmail.com</p>
+                <p>LinkedIn: linkedin.com/in/sierra-michel-</p>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
